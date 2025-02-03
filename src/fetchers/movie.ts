73587269a85eae9b710132ee0ceb71bc4db20a55
KTH8962 <gaya8962@gmail.com>
2025-02-03 20:33:06 +0900

    마이너수정

--- src/fetchers/movie.ts
+++ src/fetchers/movie.ts
@@ -4,10 +4,9 @@ export interface Movie {
 }
 
 export async function fetchMovies(title: string): Promise<Movie[]> {
-  //   const res = await fetch(
-  //     `https://omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${title}`
-  //     )
-  const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&s=${title}`)
+  const res = await fetch(
+    `https://omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${title}`
+  )
   const { Search } = await res.json()
   console.log(Search)
   return Search
